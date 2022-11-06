7858d9fd51c7010e9418a503ef603edafc6b3b66
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-06 17:47:16 -0600

    Refactoring decimal.js

--- decimal.js
+++ decimal.js
@@ -1,25 +1,20 @@
 import { getBinarySet } from "./services/getBinarySet.js"
 
 const binary = process.argv[2]
-const binaryArr = binary.trim().split("").map(item => Number(item))
+const binaryArr = binary.trim().split("").map(item => Number(item)).reverse()
+const binarySet = getBinarySet().reverse()
 
 const isValidNumber = binaryArr.every(binary => binary === 0 || binary === 1)
+
 const toDecimal = () => {
   let decimalValue = 0
-  const binarySet = getBinarySet().reverse()
-
-  binaryArr.reverse().forEach((num, numIndex) => {
-    if (num) {
-      decimalValue += binarySet[numIndex]
-    }
+  binaryArr.forEach((num, numIndex) => {
+    if (num) decimalValue += binarySet[numIndex]
   })
-  
   return decimalValue
 }
 
-if (isValidNumber) {
-  const decimal = toDecimal()
-  console.log(decimal)
-} else {
-  throw new Error("Invalid parameter. It must a be a binary number")
-}
+if (!isValidNumber) throw new Error("Invalid parameter. It must a be a binary number")
+
+const decimal = toDecimal()
+console.log(decimal)
